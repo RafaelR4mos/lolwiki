b5401189a1324ae71713612688b6083a795b2817
RafaelR4mos <20222GR.ADS0011@gravatai.ifsul.edu.br>
2023-02-07 21:10:47 -0300

    done map section and worked on header

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuHamburger = document.querySelector('.hamburger-icon img');
+
+menuHamburger.addEventListener('click', () => {
+    console.log('Click!')
+    alert('Em breve implementação do menu mobile')
+})

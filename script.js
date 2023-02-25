4a26b42e9beac96bb4f7961d7ea4328ffe1e10a6
Rafael <rafaelramos0401@gmail.com>
2023-02-25 13:54:18 -0300

    added: initial content. Homepage and know lol html and styles done

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuHamburger = document.querySelector('.hamburger-icon img');
-
-menuHamburger.addEventListener('click', () => {
-    console.log('Click!')
-    alert('Em breve implementação do menu mobile')
-})
